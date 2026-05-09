b1d2cb9cf0aaa967b2a0796bf69a248ad9d4a712
Arman Sahakyan <arman_sahakyan@hotmail.com>
2026-05-08 18:49:31 -0700

    increase body size limit for file uploads

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,9 @@ import adapter from '@sveltejs/adapter-node';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
-    bodySize: 20 * 1024 * 1024,  // 20MB
+    adapter: adapter({
+      bodySize: 20 * 1024 * 1024 // 20 MB
+    }),
   }
 };
 
